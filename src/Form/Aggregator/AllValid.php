3bfbc3395a1e5915053fe23e96940292919d4ab7
Andrey Petrov <apetrov2@avito.ru>
2016-10-30 16:27:27 +0300

    php-doc

--- src/Form/Aggregator/AllValid.php
+++ src/Form/Aggregator/AllValid.php
@@ -6,6 +6,9 @@ use Form\Aggregator;
 
 class AllValid extends Aggregator
 {
+    /**
+     * @return bool
+     */
     protected function aggregate(): bool
     {
         return !in_array(false, $this->maskValid, true);
