67657b1e0fe1638b9a966b89c417668aafcabb9e
Andrey Petrov <apetrov2@avito.ru>
2016-10-30 20:07:49 +0300

    php-doc

--- src/Form/Aggregator/SomeValid.php
+++ src/Form/Aggregator/SomeValid.php
@@ -6,6 +6,9 @@ use Form\Aggregator;
 
 class SomeValid extends Aggregator
 {
+    /**
+     * @return bool
+     */
     protected function aggregate(): bool
     {
         return in_array(true, $this->maskValid, true);
